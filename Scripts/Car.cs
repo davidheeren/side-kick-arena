37016e807bc36c9341052caac2f2ae36f0d893cf
davidheeren <davidmheeren@gmail.com>
2026-06-23 15:14:42 -0500

    flip vel reversal

--- Scripts/Car.cs
+++ Scripts/Car.cs
@@ -2,6 +2,8 @@ using Godot;
 
 public partial class Car : RigidBody2D
 {
+    // Assumes mass of 1
+
     public const float ppu = 16;
     [Export] float moveSpeed = 10;
     [Export] float verticalForce = 20;
@@ -15,10 +17,13 @@ public partial class Car : RigidBody2D
         float speed = input * moveSpeed * ppu;
         float force;
 
+        // Overspeeding
         if (LinearVelocity.X != 0 && (LinearVelocity.X - speed) * Mathf.Sign(speed) > 0)
             force = (speed - LinearVelocity.X) * overMoveDecay;
+        // Stopping
         else if (input == 0)
             force = (speed - LinearVelocity.X) * stopDecay;
+        // Moving
         else
             force = (speed - LinearVelocity.X) * moveDecay;
 
@@ -37,9 +42,15 @@ public partial class Car : RigidBody2D
         if (input == Vector2.Zero)
             input = Vector2.Up;
 
-        // TODO: If velocity < 0, then set velocity to 0 and impulse
+        Vector2 flipImpulse = input * flipSpeed * ppu;
 
-        ApplyCentralImpulse(input.Normalized() * flipSpeed * ppu);
+        // Set not moving in flip direction, cancel out negative velocity
+        if (Mathf.Sign(input.X) != Mathf.Sign(LinearVelocity.X))
+            flipImpulse.X -= LinearVelocity.X;
+        if (Mathf.Sign(input.Y) != Mathf.Sign(LinearVelocity.Y))
+            flipImpulse.Y -= LinearVelocity.Y;
+
+        ApplyCentralImpulse(flipImpulse);
     }
 
 
@@ -49,18 +60,25 @@ public partial class Car : RigidBody2D
         Vector2 input = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 
         // I don't think normalizing the dir works the best for regular movement
+        // I think its better to have each axis operate independantlly
         // if (input.Length() > overDeadzone)
         // input = input.Normalized();
 
+        // Over dead zone
         if (input.X > overDeadzone)
             input.X = 1;
+        else if (input.X < -overDeadzone)
+            input.X = -1;
         if (input.Y > overDeadzone)
             input.Y = 1;
+        else if (input.Y < -overDeadzone)
+            input.Y = -1;
 
         MoveHorizontal(input.X);
         MoveVertical(input.Y);
 
+        // Flip dir normalized
         if (Input.IsActionJustPressed("flip"))
-            Flip(input);
+            Flip(input.Normalized());
     }
 }
