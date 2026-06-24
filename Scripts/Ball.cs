b0098b4a28c21ef3d6c6aa404a0dea8a61966555
davidheeren <davidmheeren@gmail.com>
2026-06-23 19:54:57 -0500

    ball drag

---
+++ Scripts/Ball.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+public partial class Ball : RigidBody2D
+{
+    public const float ppu = 16;
+
+    [Export] float maxSpeed = 6;
+    [Export] float overDecay = 0.05f;
+    [Export] float alwaysDecay = 0.005f;
+
+    public override void _Process(double delta)
+    {
+        Vector2 force = -LinearVelocity * alwaysDecay * ppu;
+
+        if (LinearVelocity.Length() > maxSpeed * ppu)
+        {
+            GD.Print("Over");
+            force += -LinearVelocity * overDecay * ppu;
+        }
+
+        ApplyCentralForce(force * Mass);
+    }
+}
